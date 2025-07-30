0e9c30c5be77236d3f19b70df3c3df997a1e74a5
quixoticforge <sujalgyawali4@gmail.com>
2025-07-30 15:57:36 +0545

    Data compression using huffman coding completed!

--- huffman.cpp
+++ huffman.cpp
@@ -9,9 +9,11 @@
 struct dictionary{
     std::string data;
     std::string code;
-
 };
-
+struct newdictionary{
+    std::string data;
+    std::string code;
+};
 struct node
 {
     std::string data;
@@ -24,11 +26,14 @@ struct node
     node(std::string value){
         data = value;
         next = nullptr;
+        left = nullptr;
+        right=nullptr;
         freq=0;
         calc = false;
         
     }
 };
+
 class Huffman{
     private:
         std::vector<node*> nodes;
@@ -37,8 +42,8 @@ class Huffman{
         int p =0;
         int s=0;
         dictionary dict[100];
-    public:
 
+    public:
         void sort_vector_array(std::vector<node*> &nodes){
             sort(nodes.begin(), nodes.end(),[](node* a, node* b){
                 return a->freq<b->freq;
@@ -54,14 +59,11 @@ class Huffman{
                      dict[s].data = root->data;
                      dict[s].code= code;
                      s=s+1;
-            
-
                 return;
             }
             generatehuffmancodes(root->left, code+"0");
             generatehuffmancodes(root->right, code+"1");
         }
-
 
         node* HuffmanEncoding(std::string input){
             std::string answer;
@@ -139,27 +141,38 @@ class Huffman{
         node* root = nodes[0];
         return root;
         }
-
-        // void checkdict(){
-        //     for(int i =0; i<p;i++){
-                
-        //         std::cout<<"\n"<<dict[i].data<<":"<<dict[i].code<<std::endl;
-        //     }
-        // }
         void huffmanencode(std::string input){
             std::string ouptut;
+            std::string message;
+             std::string number;
             for(char ch:input){
                 for(int i =0;i<p;i++){
                     if(std::string(1,ch)==dict[i].data){
                         ouptut=ouptut+dict[i].code;
-                        // int number = stoi(dict[i].code);
-                        // std::ofstream file("encoded.bin", std::ios::binary|std::ios::app);
-                        // file.put(number);
-                        // file.close();
+                        
                     }
                 }
             }
+            std::ofstream myfile("encoded.txt");
+                myfile<<ouptut;
+                myfile.close();
             std::ofstream file("encoded.bin",std::ios::binary|std::ios::app);
+             
+             std::string lent = std::to_string(input.length());
+            
+            for(int i =0;i<s;i++){
+                 message= dict[i].data;
+                number = dict[i].code;
+                file<<message;
+                 file<<":";
+                file<<number;
+                
+                 file<<",";
+                        }
+                
+                file<<lent;
+                file<<";";
+               
 
             unsigned char byte =0;
             int count =0;
@@ -174,33 +187,107 @@ class Huffman{
             }
         }
             if(count>0){
-                byte<<=(8-count);
+                 byte<<=(8-count);
                 file.put(byte);
         }
+      
         file.close();
+          
     }
     void seebinary(){
+        newdictionary newdict[100];
+        int w=0;
+        std::string ans;
+        std::string symbol ;
+        std::string pode;
+        std::string encodedfromfile;
         std::ifstream file("encoded.bin", std::ios::binary);
         std::stringstream buffer;
-        buffer<<file.rdbuf();
-        std::string input = buffer.str();
-        for (unsigned char c : input) {
-        std::bitset<8> bits(c);
-        std::cout << bits;
-}
+        std::string len;
+        std::streampos pos;
+        std::string somevalue;
+        char ch;
+        int d=0;
+        while(file.get(ch)){
+            
+            if(ch==':'){
+               
+                newdict[d].data = somevalue;
+               
+                somevalue.clear();
+                continue;
+            }
+            if(ch==','){
+                newdict[d].code = somevalue;
+                somevalue.clear();
+                d=d+1;
+                continue;
+            }
+            if(ch==';'){
+                len = somevalue;
+                pos = file.tellg();
+                break;
+            }
+             somevalue = somevalue+ch;
+             
+
+        }
+     
+        file.clear();
+        file.seekg(pos);
+        while (file.get(ch)){
+          std::bitset<8> bits(ch);
+           encodedfromfile +=bits.to_string();
+        }
 
         file.close();
+
+        std::string s(1,'0');
+        node* decoderoot = new node(s);
+        for(int i =0;i<d;i++){
+            symbol = newdict[i].data;
+            pode = newdict[i].code;
+           
+            node* current = decoderoot;
+
+            for(char bit :pode){
+                if(bit =='0'){
+                    if(!current->left) current->left = new node(s);
+                    current = current->left;
+                }else if(bit=='1'){
+                    if(!current->right) current->right = new node(s);
+                    current = current->right;
+                }
+            }
+            current->data = symbol;
+
+        }
+        
+        node* temp = decoderoot;
+        int count =0;
+        int leng = stoi(len);
+        
+        for(int i =0;count<leng;i++){
+            if(encodedfromfile[i]=='0'){
+                temp = temp->left;
+            }
+            else{
+                 temp = temp->right;
+
+            }
+            if(temp->left == nullptr and temp->right == nullptr){
+                ans+=temp->data;
+                count = count+1;
+                temp = decoderoot;
+            }
+        }
+        ans = ans+'\0';
+        std::cout<<ans<<std::endl;
     }
-
-};
-
-
+ };
 int main(){
     Huffman h;
     node* root;
-    // std::string input;
-    // std::cout<<"Enter the sequence you want to encode"<<std::flush;
-    // std::getline(std::cin, input);
     std::ifstream file("sample.txt");
     std::stringstream buffer;
     buffer<<file.rdbuf();
@@ -210,8 +297,5 @@ int main(){
     root = h.HuffmanEncoding(input);
     h.generatehuffmancodes(root, "");
     h.huffmanencode(input);
-    h.seebinary();
-
+    h.seebinary(); 
 }
-// #1111011111100100000010111101011100001101001000110100110100101010101101100111110100111000
-// #11110111111001000000101111010111000011010010001101001101001010101011011001111101001110
